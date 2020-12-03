7f06bb80c2a4b967755b24cb5ff6c0aaed36e517
YeonGyu Jeong <81887821@hanmail.net>
2020-12-03 15:28:40 +0900

    Updated fish.config

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -4,8 +4,10 @@ set --export XDG_RUNTIME_DIR /run/user/(id -u)
 set --export GPG_TTY (tty)
 
 
-if [ -d $HOME/bin ]
-  set PATH $PATH "$HOME/bin"
+test -d $HOME/bin; and set PATH $PATH "$HOME/bin"
+if [ (id -u) = 0 ]
+  test -d /usr/sbin -a ! -L /usr/sbin; and set PATH $PATH /usr/sbin
+  test -d /usr/local/sbin -a ! -L /usr/local/sbin; and set PATH $PATH /usr/local/sbin
 end
 
 
