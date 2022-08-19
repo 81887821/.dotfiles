cd265253a6158e252021f76efb123d049dfbf8c4
YeonGyu Jeong <81887821@hanmail.net>
2022-08-19 17:14:04 +0900

    Added git abbreviations

---
+++ home/.config/fish/conf.d/git.fish
@@ -0,0 +1,5 @@
+abbr --add gf "git fetch --all --prune"
+abbr --add gff "git merge --ff-only origin/(git branch --show-current)"
+abbr --add glo "git log --graph --all"
+abbr --add gp "git push"
+abbr --add gpp "git push origin (git branch --show-current)"
