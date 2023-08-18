666bb8f571e592f84071834e69d696df48e98654
YeonGyu Jeong <81887821@hanmail.net>
2023-08-18 10:14:47 +0900

    tfswitch: per session version switching

--- home/.config/fish/conf.d/tfswitch.fish
+++ home/.config/fish/conf.d/tfswitch.fish
@@ -1,10 +1,11 @@
-set TFSWITCH_PATH $HOME/.local/tfswitch
-set PATH $TFSWITCH_PATH $PATH
+set TFSWITCH_PATH /tmp/tfswitch-$USER
+test -d $TFSWITCH_PATH; or mkdir $TFSWITCH_PATH
 
 function switch_terraform --on-event fish_postexec
-    if string match --regex '^cd\s' "$argv" > /dev/null
-        if count *.tf > /dev/null; and grep -c "required_version" *.tf > /dev/null
-            command tfswitch --bin=$TFSWITCH_PATH/terraform
+    if string match --regex '^cd\s' "$argv" >/dev/null
+        if count *.tf >/dev/null; and grep -c "required_version" *.tf >/dev/null
+            set -f TERRAFORM_PATH $TFSWITCH_PATH/$fish_pid
+            command tfswitch --bin=$TERRAFORM_PATH; and alias terraform $TERRAFORM_PATH
         end
     end
 end
