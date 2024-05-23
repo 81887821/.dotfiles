d844b2ac66c98df36add970cf4c43ba578580748
YeonGyu Jeong <81887821@hanmail.net>
2024-05-23 10:35:34 +0900

    Fix error on `kgnok` alias

--- home/.config/fish/conf.d/kube_aliases.fish
+++ home/.config/fish/conf.d/kube_aliases.fish
@@ -1,4 +1,4 @@
 abbr --add kns "kubens"
 abbr --add kcx "kubectx"
 abbr --add kgnocas "kubectl get nodes -o custom-columns=Name:.metadata.name,InstanceGroup:.metadata.labels.aws/instance-group,KubeletVersion:.status.nodeInfo.kubeletVersion,Unschedulable:.spec.unschedulable --sort-by .metadata.labels.aws/instance-group"
-abbr --add kgnok "kubectl get nodes -o custom-columns=Name:.metadata.name,NodePool:.metadata.labels.karpenter\\.sh/nodepool,InstanceType:.metadata.labels.beta\\\\.kubernetes\\\\.io/instance-type,KubeletVersion:.status.nodeInfo.kubeletVersion,Unschedulable:.spec.unschedulable --sort-by .metadata.labels.karpenter\\\\.sh/nodepool"
+abbr --add kgnok "kubectl get nodes -o custom-columns=Name:.metadata.name,NodePool:.metadata.labels.karpenter\\\\.sh/nodepool,InstanceType:.metadata.labels.beta\\\\.kubernetes\\\\.io/instance-type,KubeletVersion:.status.nodeInfo.kubeletVersion,Unschedulable:.spec.unschedulable --sort-by .metadata.labels.karpenter\\\\.sh/nodepool"
